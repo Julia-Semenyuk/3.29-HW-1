c2a0e4d8c43f1939910a7acd752d8bb935de300e
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-09-14 01:15:23 +0900

    1

---
+++ 3.29 HW 1/ViewControllers/SetUpAnimationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  3.29 HW 1
+//
+//  Created by Юлия on 13.09.2022.
+//
+
+import UIKit
+import SpringAnimation
+
+final class setUpAnimationViewController: UIViewController {
+
+    @IBOutlet var animationView: UIView!
+    
+
+   
+   
+    
+    
+
+    @IBAction func runAnimationButton() {
+   
+    }
+    
+    
+}
+
