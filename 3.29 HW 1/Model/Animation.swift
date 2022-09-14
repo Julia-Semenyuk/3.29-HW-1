93d8f3c898e2761e17cfc24b7c9bb8d5529be2d5
Julia Semenyuk <julia_semenyuk@internet.ru>
2022-09-14 13:38:04 +0900

    1.1

--- 3.29 HW 1/Model/Animation.swift
+++ 3.29 HW 1/Model/Animation.swift
@@ -9,58 +9,13 @@ import Foundation
 import SpringAnimation
 
 struct Animation {
-    let present: AnimationPreset
-    let curve: AnimationCurve
-    let force: Double
-    let duration: Double
-    let delay: Double
     
-}
+    let present: AnimationPreset?
+    let curve: AnimationCurve?
+    let force: Double?
+    let duration: Double?
+    let delay: Double?
 
-extension Animation {
-    static func getAnimation() -> [Animation] {
-        
-        var animations: [Animation] = []
-        let animationsPresent = AnimationPreset.allCases
-        let animationsCurve = AnimationCurve.allCases
-        
-        let iterationCount = min(
-            animationsPresent.count,
-            animationsCurve.count
-        )
-        
-        for indexAnimation in 0...(iterationCount) {
-            let animation = Animation(
-                present: animationsPresent[indexAnimation],
-                curve: animationsCurve[indexAnimation],
-                force: getRandomForce(),
-                duration: getRandomDuration(),
-                delay: getRandomDelay()
-            )
-            animations.append(animation)
-        }
-        
-        return animations
-    
-    }
-    
-    private static func getRandomForce() -> Double {
-        let force = Double.random(in: 0.2...3)
-        return force
-        
-    }
-    
-    private static func getRandomDuration() -> Double {
-        let duration = Double.random(in: 0.2...3)
-        return duration
-    }
-    
-    private static func getRandomDelay() -> Double {
-        let delay = Double.random(in: 0.2...3)
-        return delay
-    }
-    
 }
-
 
 
--- 3.29 HW 1/ViewControllers/SetUpAnimationViewController.swift
+++ 3.29 HW 1/ViewControllers/SetUpAnimationViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 import SpringAnimation
 
 final class setUpAnimationViewController: UIViewController {
-
+   
     @IBOutlet var animationView: UIView!
+    @IBOutlet var runAnimationButton: UIButton!
     
-
-   
-   
+    private let animations = Animation.getAnimation()
     
-    
-
-    @IBAction func runAnimationButton() {
-   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpAnimatiomView()
     }
     
+    @IBAction func runAnimationButtonPressed() {
+        
+    }
     
+    private func setUpAnimatiomView() {
+        
+    }
 }
-
